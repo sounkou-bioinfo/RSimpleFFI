95b2bc59dd733017d20899944333431246808e2a
sounkou-bioinfo <sounkoutoure@gmail.com>
2025-12-05 18:00:33 +0100

    fix: codegen

--- R/04-alloc.R
+++ R/04-alloc.R
@@ -54,6 +54,24 @@ S7::method(ffi_alloc, EnumType) <- function(type, n = 1L) {
   .Call("R_alloc_typed_buffer", type@underlying_type@ref, as.integer(n))
 }
 
+#' @export
+S7::method(ffi_alloc, S7::class_list) <- function(type, n = 1L) {
+  # Detect bitfield accessor list: must have 'pack', 'unpack', 'get', 'set' functions and 'base_type'
+  is_bitfield <- all(c("pack", "unpack", "get", "set") %in% names(type)) && !is.null(type$base_type)
+  if (is_bitfield) {
+    base_type <- type$base_type
+    if (!S7::S7_inherits(base_type, FFIType)) {
+      stop("base_type must be an FFIType object")
+    }
+    if (length(n) != 1 || !is.numeric(n) || n < 1) {
+      stop("n must be a positive integer")
+    }
+    .Call("R_alloc_typed_buffer", base_type@ref, as.integer(n))
+  } else {
+    stop("Don't know how to allocate for this list type. If this is a bitfield accessor, it must have 'pack', 'unpack', 'get', 'set', and 'base_type'.")
+  }
+}
+
 #' Get element from struct array
 #'
 #' Returns a pointer to the i-th struct in a contiguous array of structs.
@@ -67,7 +85,7 @@ S7::method(ffi_alloc, EnumType) <- function(type, n = 1L) {
 #' @examples
 #' \dontrun{
 #' Point <- ffi_struct(x = ffi_int(), y = ffi_int())
-#' points <- ffi_alloc(Point, 10L)  # array of 10 Points
+#' points <- ffi_alloc(Point, 10L) # array of 10 Points
 #' p3 <- ffi_get_element(points, 3L, Point)
 #' ffi_set_field(p3, "x", 100L, Point)
 #' }
